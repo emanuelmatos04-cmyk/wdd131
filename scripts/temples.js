f439cb6a9b84b700e44112bb49aa707f4133b523
emanuelmatos04 <ematosfrancisco@byupathway.du>
2026-05-17 01:25:08 -0400

    W2C5

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,17 @@
 // Footer dynamic year and last modified
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
-
+ 
 // Hamburger menu toggle
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
-
+ 
 hamburger.addEventListener("click", () => {
-  if (navMenu.style.display === "flex") {
-    navMenu.style.display = "none";
-    hamburger.textContent = "☰";
-  } else {
-    navMenu.style.display = "flex";
+  navMenu.classList.toggle("open");
+ 
+  if (navMenu.classList.contains("open")) {
     hamburger.textContent = "✖";
+  } else {
+    hamburger.textContent = "☰";
   }
 });
